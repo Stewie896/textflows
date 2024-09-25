ceb72ff7391c5eb4fe13969b01098cf4750576c2
abhiral chhetri <abhiralchhetri6@gmail.com>
2024-09-25 20:45:02 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter  } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>x
+  <HashRouter>
   <StrictMode>
     <App />
   </StrictMode>,
